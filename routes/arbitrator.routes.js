cac6127f6e3ab91ec0e71056edacd621bdc831e1
Mehedi Hasan <111342979+mehedihasan101@users.noreply.github.com>
2025-11-13 00:26:27 +0600

    add arbitrator profile update

--- routes/arbitrator.routes.js
+++ routes/arbitrator.routes.js
@@ -20,4 +20,44 @@ router.get("/all-arbitrators", verifyToken, async (req, res) => {
     res.send(result);
 });
 
+router.get("/ArbitratorProfile", verifyToken, async (req, res) => {
+    const { email } = req.query;
+    const arbitrator = await arbitratorCollection.findOne({ email });
+    res.send(arbitrator);
+});
+
+// update lawyer profile
+router.patch("/ArbitratorProfile/:email", verifyToken, async (req, res) => {
+    const { email } = req.params;
+    const data = req.body;
+
+    // Remove _id from the update data to prevent immutable field modification
+    const { _id, ...updateData } = data;
+
+    // If there's nothing left to update after removing _id
+    if (Object.keys(updateData).length === 0) {
+        return res.status(400).send({
+            success: false,
+            error: "No valid fields to update",
+        });
+    }
+
+    const result = await arbitratorCollection.updateOne(
+        { email: email },
+        { $set: updateData }
+    );
+
+    if (result.matchedCount === 0) {
+        return res.status(404).send({
+            success: false,
+            error: "Arbitrator not found",
+        });
+    }
+
+    res.send({
+        success: true,
+        message: "Arbitrator profile updated successfully",
+    });
+});
+
 module.exports = router;
